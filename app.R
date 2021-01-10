f86f27507689ef37ac2c0e64cd35906444b662b0
codyastubbs <castubbs3@gmail.com>
2021-01-09 21:00:45 -0500

    Adding first published version of app.

--- app.R
+++ app.R
@@ -1,49 +1,65 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(dplyr)
+library(lubridate)
+# Define basic save and load data functions
+library(googlesheets4)
+library(googledrive)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
+table <- "https://docs.google.com/spreadsheets/d/1B-VWdlKnxVkN4I53JuO9uHJvOxyGckTz-iGMvhjzFNI/edit?usp=sharing"
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
+#drive_auth(cache = ".secrets") #for the first time running the app in R to get the OAuth token
+gs4_auth(cache = ".secrets", email = TRUE, use_oob = TRUE)
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
-)
-
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
+saveData <- function(data) {
+    # Add the data as a new row
+    sheet_append(table, data)
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+loadData <- function() {
+    # Read the table
+    read_sheet(table)
+}
+
+# Shiny app with 3 fields that the user can submit data for
+shinyApp(
+    ui = fluidPage(
+        textInput("date", "Date (e.g.: 2020-12-09)", ""),
+        textInput("path", "Career Path", ""),
+        textInput("step", "Step Number", ""),
+        textInput("course", "Course Number", ""),
+        textInput("duration", "Duration (Minutes)", ""),
+        textInput("notes", "Notes:", ""),
+        actionButton("submit", "Submit"),
+        DT::dataTableOutput("responses", width = 300), tags$hr()
+        # ,
+        # plotOutput("plot", width = "400px")
+    ),
+    server = function(input, output, session) {
+        
+        # Whenever a field is filled, aggregate all form data
+        formData <- reactive({
+            data <- tibble(data.entry.date = as_date(Sys.Date()),
+                           date = input$date %>% as_date(),
+                           path = input$path,
+                           step = input$step,
+                           course = input$course,
+                           duration = input$duration,
+                           notes = input$notes)
+            
+        })
+        
+        # When the Submit button is clicked, save the form data
+        observeEvent(input$submit, {
+            saveData(formData())
+        })
+        
+        # Show the previous responses
+        # (update with current response when Submit is clicked)
+        output$responses <- DT::renderDataTable({
+            input$submit
+            loadData()
+        })     
+        
+        # output$plot <- renderPlot(plot(x = formData$item, y = formData$weight), res = 100)
+    }
+)
